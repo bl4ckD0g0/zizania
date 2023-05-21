4bdc4038a8b5ff89ec55a4fc40bad753eaff9d7a
bl4ckD0g0 <alexheavy1@gmail.com>
2023-05-21 21:20:03 +0200

    List base

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
 use druid::widget::Label;
+use druid::widget::Flex;
 use druid::{AppLauncher, Widget, WindowDesc};
+use druid::widget::{Container, Split};
+use druid::Color;
 
 fn build_ui() -> impl Widget<()> {
-    Label::new("Hello world")
+    Split::columns(
+        Container::new(
+            Flex::column()
+                .with_flex_child(Label::new("first item"), 1.0)
+                .with_flex_child(Label::new("second item"), 1.0)
+                .with_flex_child(Label::new("third item"), 1.0)
+                .with_flex_child(Label::new("fourth item"), 1.0),
+        )
+        .border(Color::grey(0.6), 2.0),
+        Container::new(
+            Flex::column()
+                .with_flex_child(Label::new("Button placeholder"), 1.0)
+                .with_flex_child(Label::new("Textbox placeholder"), 1.0),
+        )
+        .border(Color::grey(0.6), 2.0),
+    )
 }
 
 fn main() {
